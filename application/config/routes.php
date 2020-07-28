083fdb1779b330af502fe05d58d5836b849e88ff
Rizuki Ryumada <rizki3366@msn.cn>
2020-07-28 14:33:24 +0700

    change the routing of healthreport

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,4 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 // custim route
-$route['checkhealth'] = 'direct/checkhealth';
+$route['healthreport'] = 'direct/checkhealth';
