b5b6257e2ef28baa8ad2840937d9db5ac5fd34cc
Rizuki Ryumada <rizki3366@msn.cn>
2020-09-25 16:34:09 +0700

    progress pmk create index function #1

---
+++ application/controllers/Pmk.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pmk extends SpecialUserAppController {
+
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Pmk.php */
